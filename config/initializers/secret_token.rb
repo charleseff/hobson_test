dbe98e880744abad6559ae6d592cc4b0331fba14
Charles Finkel <tech_ops+charles@change.org>
2012-10-26 11:53:17 -0700

    stuff

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HobsonTest::Application.config.secret_token = 'b78d298dc07a2ff5cfa6f4361c37c0d2e226e4e489bd0d79aa34edc83a587f5c09034d464a0debbedb438ba22a249ae7e49925b04d148e11be1115fdc3fc710a'
---
+++ spec/lib/foo_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+describe 'foo' do
+  it "should be true" do
+    true.should == true
+  end
+
+end
